99ef50729bbaa8b19f1b923c18cfdac73b6cdd67
Chris Patrick <atomize101@gmail.com>
2020-10-18 10:37:03 -0400

    Adding in some CSS for img

--- src/App.js
+++ src/App.js
@@ -1,34 +1,36 @@
-import React,  { useState } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-
-  const [products] = useState([
-    {
-      name: 'AA Battery',
-      cost: '$2.99',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQssvRf0kYLmdHTByPxbsbRK0QmGXFhCqRtuUZErTa2nozTRNwU-gycXAfcnoT_JC_DF6yL_Zni&usqp=CAc'
-    },
-    {
-      name: 'Blanket',
-      cost: '$19.99',
-      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBv_F6IkhsSSifdmupiY05jRRrVHYbutAXe368YQ8FKHJc_8mPJ7dplFJ3LKR_LEI3vRZA2Qo&usqp=CAc'
-    }
-  ]);
-  return (
-    <div className="App">
-    <h1>Products</h1>
-    {products.map((product) => (
-      <div>
-        <h3>{product.name}</h3>
-    <h4>{product.cost}}</h4>
-        <img src={product.image} />
-        <button>Add to Cart</button>
-  </div>
-    
-    ))}
-          </div>
-  );
+	const [products] = useState([
+		{
+			name: 'AA Battery',
+			cost: '$2.99',
+			image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQssvRf0kYLmdHTByPxbsbRK0QmGXFhCqRtuUZErTa2nozTRNwU-gycXAfcnoT_JC_DF6yL_Zni&usqp=CAc',
+		},
+		{
+			name: 'Blanket',
+			cost: '$19.99',
+			image:
+				'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBv_F6IkhsSSifdmupiY05jRRrVHYbutAXe368YQ8FKHJc_8mPJ7dplFJ3LKR_LEI3vRZA2Qo&usqp=CAc',
+		},
+	]);
+	return (
+		<div className="App">
+			<h1>Products</h1>
+      <div className="products">
+			{products.map((product, index) => (
+				<div className="product" key={index} >
+					<h3>{product.name}</h3>
+					<h4>{product.cost}</h4>
+					<img src={product.image} alt={product.name} />
+					<button>Add to Cart</button>
+				</div>
+			))}
+      </div>
+		</div>
+	);
 }
 
 export default App;
