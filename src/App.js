2d1ec6d80e28eff611ad4e0ca6ae6eeca9d99ac6
Chris Patrick <atomize101@gmail.com>
2020-10-18 08:32:45 -0400

    Aplying useState and looping over products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [products] = useState([
+    {
+      name: 'AA Battery',
+      cost: '$2.99',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQssvRf0kYLmdHTByPxbsbRK0QmGXFhCqRtuUZErTa2nozTRNwU-gycXAfcnoT_JC_DF6yL_Zni&usqp=CAc'
+    },
+    {
+      name: 'Blanket',
+      cost: '$19.99',
+      image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBv_F6IkhsSSifdmupiY05jRRrVHYbutAXe368YQ8FKHJc_8mPJ7dplFJ3LKR_LEI3vRZA2Qo&usqp=CAc'
+    }
+  ]);
   return (
     <div className="App">
-      <h1>Products</h1>
+    <h1>Products</h1>
+    {products.map((product) => (
       <div>
-        <h3>AA Battery</h3>
-        <h4>2.99</h4>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQssvRf0kYLmdHTByPxbsbRK0QmGXFhCqRtuUZErTa2nozTRNwU-gycXAfcnoT_JC_DF6yL_Zni&usqp=CAc" />
+        <h3>{product.name}</h3>
+    <h4>{product.cost}}</h4>
+        <img src={product.image} />
         <button>Add to Cart</button>
-      </div>
-
-      <div>
-        <h3>Blanket</h3>
-        <h4>19.99</h4>
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBv_F6IkhsSSifdmupiY05jRRrVHYbutAXe368YQ8FKHJc_8mPJ7dplFJ3LKR_LEI3vRZA2Qo&usqp=CAc" />
-        <button>Add to Cart</button>
-      </div>
-
-    </div>
+  </div>
+    
+    ))}
+          </div>
   );
 }
 
