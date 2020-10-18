954cb6b187d612041cf34e8c76c13b23f811d8e9
Chris Patrick <atomize101@gmail.com>
2020-10-18 11:46:24 -0400

    Refactor and constants

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 
+const PAGE_PRODUCTs = 'products';
+const PAGE_CART = 'cart';
+
 function App() {
-  const [cart, setCart] = useState([]);
+	const [cart, setCart] = useState([]);
+	const [page, setPage] = useState('products');
+
 	const [products] = useState([
 		{
 			name: 'AA Battery',
@@ -16,29 +21,52 @@ function App() {
 			image:
 				'https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBv_F6IkhsSSifdmupiY05jRRrVHYbutAXe368YQ8FKHJc_8mPJ7dplFJ3LKR_LEI3vRZA2Qo&usqp=CAc',
 		},
-  ]);
-  
-  const addToCart = (product) => {
-    console.log('adding to cart')
-    setCart([...cart, product]);
-  }
+	]);
+
+	const addToCart = (product) => {
+		console.log('adding to cart');
+		setCart([...cart, product]);
+	};
+
+	const renderCart = () => (
+		<>
+			<h1>Cart</h1>
+			<div className="products">
+				{products.map((product, index) => (
+					<div className="product" key={index}>
+						<h3>{product.name}</h3>
+						<h4>{product.cost}</h4>
+						<img src={product.image} alt={product.name} />
+						<button onClick={() => addToCart(product)}>Add to Cart</button>
+					</div>
+				))}
+			</div>
+		</>
+  );
+
+	const renderProducts = () => (
+		<>
+			<h1>Products</h1>
+			<div className="products">
+				{products.map((product, index) => (
+					<div className="product" key={index}>
+						<h3>{product.name}</h3>
+						<h4>{product.cost}</h4>
+						<img src={product.image} alt={product.name} />
+						<button onClick={() => addToCart(product)}>Add to Cart</button>
+					</div>
+				))}
+			</div>
+		</>
+  );
 
 	return (
 		<div className="App">
-      <header>
-        <button>Go to Cart ({cart.length})</button>
-      </header>
-			<h1>Products</h1>
-      <div className="products">
-			{products.map((product, index) => (
-				<div className="product" key={index} >
-					<h3>{product.name}</h3>
-					<h4>{product.cost}</h4>
-					<img src={product.image} alt={product.name} />
-					<button onClick={() => addToCart(product)}>Add to Cart</button>
-				</div>
-			))}
-      </div>
+			<header>
+				<button>Go to Cart ({cart.length})</button>
+			</header>
+			{page === 'products' && renderProducts()}
+			{page === 'cart' && renderCart()}
 		</div>
 	);
 }
