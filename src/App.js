e419cba7b01b393edb67d72e88c3f76d7bcbee6a
Chris Patrick <atomize101@gmail.com>
2020-10-18 08:19:48 -0400

    Setting up initial html

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <h1>Products</h1>
+      <div>
+        <h3>AA Battery</h3>
+        <h4>2.99</h4>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQssvRf0kYLmdHTByPxbsbRK0QmGXFhCqRtuUZErTa2nozTRNwU-gycXAfcnoT_JC_DF6yL_Zni&usqp=CAc" />
+        <button>Add to Cart</button>
+      </div>
+
+      <div>
+        <h3>Blanket</h3>
+        <h4>19.99</h4>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSBv_F6IkhsSSifdmupiY05jRRrVHYbutAXe368YQ8FKHJc_8mPJ7dplFJ3LKR_LEI3vRZA2Qo&usqp=CAc" />
+        <button>Add to Cart</button>
+      </div>
+
+    </div>
   );
 }
 
